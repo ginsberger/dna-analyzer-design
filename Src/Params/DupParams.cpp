659e1bdaa0e4844b9084c355f7c079eff00b41da
bracha ginsberger <z618149948@gmail.com>
2020-07-09 05:11:20 -0700

    add DupParams

---
+++ Src/Params/DupParams.cpp
@@ -0,0 +1,51 @@
+
+#include "DupParams.h"
+
+#include <sstream>
+#include "NewParams.h"
+#include "../Exeptions/InValidParams.h"
+#include "../DNA/DNAContainer.h"
+
+
+DupParams::DupParams(const std::string& commandLine)
+{
+    IParams::parseCommand(commandLine);
+
+    if(!isValidParams())
+    {
+        throw InValidParams();
+    }
+}
+
+bool DupParams::isValidParams()
+{
+    __gnu_cxx::hash_map<std::string, size_t>* nameCounter = getNameCounter();
+    if(1 < IParams::getParams().size())
+    {
+        return false;
+    }
+    if(1 == IParams::getParams().size()) //without name
+    {
+        std::string prevName = DNAContainer::getDnaData().find(atoi(IParams::getParams()[0].c_str()))->getName();
+        std::stringstream name;
+        name << prevName << '_' << ++(*nameCounter)[prevName];
+        (*nameCounter)[name.str()] = 1; // add the new name to the list
+        IParams::addParam(name.str());
+        return true;
+    }
+
+    if(2 == IParams::getParams().size()) //with name
+    {
+        std::string name = IParams::getParams()[1];
+        if('@' == name[0])
+            if(0 != (*nameCounter)[name])
+            {
+                std::stringstream newName;
+                newName << name << ++(*nameCounter)[name];
+                (*nameCounter)[newName.str()] = 1;// add the new name to the list
+                IParams::getParams()[1] = newName.str();
+            }
+        return true;
+    }
+    return false;
+}
---
+++ Src/Params/DupParams.h
@@ -0,0 +1,16 @@
+
+#ifndef DNASEQUENCE_DUPPARAMS_H
+#define DNASEQUENCE_DUPPARAMS_H
+
+#include "IParams.h"
+class DupParams: public IParams {
+public:
+    DupParams(const std::string& commandLine);
+
+protected:
+    bool isValidParams();
+
+};
+
+
+#endif //DNASEQUENCE_DUPPARAMS_H
