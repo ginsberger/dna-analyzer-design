c7fbb8984ed53d868889fbc360f8df7b539c8723
bracha ginsberger <z618149948@gmail.com>
2020-09-15 16:50:33 -0700

    save in params without the prefix(#/@)

---
+++ Src/CLI/CommandParams/SequenceManipulationCommandsParams/Slice/SliceParams.h
@@ -0,0 +1,14 @@
+//
+// Created by a on 9/15/20.
+//
+
+#ifndef DNASEQUENCE_SLICECOMMAND_H
+#define DNASEQUENCE_SLICECOMMAND_H
+
+
+class SliceCommand {
+
+};
+
+
+#endif //DNASEQUENCE_SLICECOMMAND_H
--- Src/CLI/Commands/SequenceCreationCommands/Load/LoadCommand.cpp
+++ Src/CLI/Commands/SequenceCreationCommands/Load/LoadCommand.cpp
@@ -9,16 +9,21 @@ std::string LoadCommand::run(const IParams* params)
     std::cout<<params->getParams()[0].c_str() << std::endl;
     FileReader reader(params->getParams()[0].c_str());
     StringReader s(reader.read());
-//    std::cout << "aaaaaaa" << reader.read() << " bbbbbb" << s.read();
     DNAMetaData* dna = new DNAMetaData(params->getParams()[1],s);
     (DNAContainer::getDnaData()).insert(dna);
 
     std::stringstream ss;
     size_t id = DNAContainer::getDnaData().getIdByName(params->getParams()[1]);
-    ss <<"[" << id << "] ";
-    ss << params->getParams()[1] << ": "; // name
-    ss << DNAContainer::getDnaData().operator[](id)->getDNA().getDnaSequence();
-//    ss << reader.read(); // dna
+    std::string name = params->getParams()[1];
+    std::string DNA = DNAContainer::getDnaData()[id]->getDNA().getDnaSequence();
+    size_t len = DNA.size();
+    if(len >= 40)
+    {
+        DNA =  DNA.substr(0, 32) + "..." + DNA.substr(len - 3, len);
+    }
+    ss <<"[" << id << "] " << name << ": " << DNA;
+
     return ss.str();
 }
+
 
