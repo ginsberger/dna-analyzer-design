3ef45fa383fa29feea163e4b65056b3a66d927db
bracha ginsberger <z618149948@gmail.com>
2020-09-16 17:04:02 -0700

    add SaveParams class

---
+++ Src/CLI/CommandParams/SequenceManagementCommandsParams/Save/SaveParams.cpp
@@ -0,0 +1,58 @@
+
+#include "SaveParams.h"
+#include "../../../../Exeptions/InValidParam/TooFewArguments.h"
+#include "../../../../Exeptions/InValidParam/IdOrNameError.h"
+
+
+SaveParams::SaveParams(const std::string& commandLine)
+{
+    IParams::parseCommand(commandLine);
+
+    validetParams();
+}
+
+
+void SaveParams::validetParams()
+{
+    sequenceNameMap& nameCounter = IParams::getNameCounter();
+    if(IParams::getParams().empty())
+    {
+        throw TooFewArguments();
+    }
+
+    if(1 == IParams::getParams().size()) // without filename
+    {
+
+        DNAMetaData* dnaMetaDataById = getMetaDataByIdOrName();
+        IParams::addParam(dnaMetaDataById->getName() + ".rawdna");
+    }
+
+    if(2 == IParams::getParams().size()) // with filename
+    {
+        getMetaDataByIdOrName();
+        IParams::getParams()[1] = IParams::getParams()[1] + ".rawdna";
+    }
+}
+
+
+DNAMetaData *SaveParams::getMetaDataByIdOrName() {
+    DNAMetaData *dnaMetaDataById = DNAContainer::getDnaData().operator[](
+            atoi(IParams::getParams()[0].substr(1).c_str()));
+    size_t id = DNAContainer::getDnaData().operator[](IParams::getParams()[0].substr(1));
+
+    bool isId = dnaMetaDataById && IParams::getParams()[0][0] == '#';
+    bool isName = id && IParams::getParams()[0][0] == '@';
+    if (!isId && !isName)
+    {
+        throw IdOrNameError();
+    }
+
+    if(isName)
+    {
+        dnaMetaDataById = DNAContainer::getDnaData().operator[](id);
+    }
+
+    return dnaMetaDataById;
+}
+
+
