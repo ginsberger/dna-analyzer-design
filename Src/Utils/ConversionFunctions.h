8189a754622f5f6d1be4e614063e2f1084888f1b
bracha ginsberger <z618149948@gmail.com>
2020-09-24 14:35:08 -0700

    inline the functions

--- Src/Utils/ConversionFunctions.h
+++ Src/Utils/ConversionFunctions.h
@@ -5,14 +5,14 @@
 #include <iostream>
 #include <sstream>
 
-std::string numToString(size_t num)
+inline std::string numToString(size_t num)
 {
     std::stringstream ss;
     ss << num;
     return ss.str();
 }
 
-size_t stringToNum(const std::string& string)
+inline size_t stringToNum(const std::string& string)
 {
     std::stringstream ss(string);
     size_t num;
